814ec7e917315812575b9ae75a0885c0cd636a07
noudadrichem <info@noudadrichem.com>
2018-10-10 18:04:07 +0200

    blinking led, off button not working yet

--- button-toggle.py
+++ button-toggle.py
@@ -1,27 +1,53 @@
 import RPi.GPIO as GPIO
+import threading
 from time import sleep
 
-ledPin = 8
+alarmPin= 8
+startPin = 12
 btnPin = 10
 interval = 0.5
+isBlinking = True 
+
+
+
+BUTTON_PRESSED = False
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(alarmPin, GPIO.OUT, initial=False)
+GPIO.setup(startPin, GPIO.OUT, initial=True)
+
+def setInterval(function, intervalDuration):
+    event = threading.Event()
+    while not event.wait(intervalDuration):
+        function()
 
 
-def sendOutSignal(chan):
-    state = not GPIO.input(ledPin)
+
+def sendOutSignal(pin):
+    sleep(.1)
+    state = not GPIO.input(alarmPin)
+
+    if GPIO.input(pin):
+        BUTTON_PRESSED = True
+    else:
+        BUTTON_PRESSED = False
 
     if state:
-        while True:
-            GPIO.output(ledPin, GPIO.HIGH)
-            sleep(1)
-            GPIO.output(ledPin, GPIO.LOW)
-            sleep(1)
+        if isBlinking:
+            # setInterval(blink, interval)
+            GPIO.output(alarmPin, GPIO.HIGH)
+        else:
+            GPIO.output(alarmPin, GPIO.LOW)
     else:
-        GPIO.output(ledPin, GPIO.LOW)
+        GPIO.output(alarmPin, GPIO.LOW)
 
+
+
+def blink():
+    global isBlinking
+    isBlinking = not isBlinking
 
 
 def start():
@@ -32,10 +58,12 @@ def start():
     )
 
 
+
 try:
     start()
+    input('entor\n')
 
-except KeyboardInterrupt:
+except:
     GPIO.cleanup()
 
 
