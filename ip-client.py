3fbecf0f215f47116f2aee34350c7479e7ef5821
Noud Adrichem <info@noudadrichem.com>
2018-10-15 11:59:25 +0200

    update ip-client from pi maarten

--- ip-client.py
+++ ip-client.py
@@ -1,13 +1,31 @@
 import socket
+import RPi.GPIO as GPIO
+from time import sleep
 
 HOST = '192.168.42.1'
 PORT = 6677
 
-s = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
-s.connect((HOST, PORT))
-s.sendall('Hello PINOUD')
-data = s.recv(1024)
+btnPin = 8
+def setup():
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)                                           
+print('setup')
 
-print('recieved', repr(data))
+def aanOfUitMan(ch):
+	print('alarm gaat af')
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                           
+	s.connect((HOST, PORT))
+	s.sendall('True')
 
 
+
+def start():
+	GPIO.add_event_detect(btnPin, GPIO.FALLING, callback=aanOfUitMan)                               
+
+setup()
+
+try:
+	start()
+	input()
+except:
+	print('doeiiii')
