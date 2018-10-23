ff426f169ae03ef5bcd2887c26c6bd7e3c239225
noudadrichem <info@noudadrichem.com>
2018-10-23 15:35:53 +0200

    server tweaks

--- ip-server.py
+++ ip-server.py
@@ -16,36 +16,56 @@ server.bind(('', port))
 server.listen(4)
 print('socket listening on 4')
 client_socket, client_address = server.accept()
-
+isAlarmRining = False
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(alarmPin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(startPin, GPIO.OUT, initial=True)
+print('has setup')
+
+def init(): 
+    print('je kan het alarm nu uitzetten')
+    GPIO.add_event_detect(
+        btnPin, 
+        GPIO.RISING, 
+        callback=zetAlarmUit
+    )
 
 
-while True:
-    client, addr = server.accept()
-    client.send('True'.encode())
-    isAlarmRinging = client.recv(1024)
-    
+def laatAlarmAfGaan():
+    init()
+    print('het alarm gaat zo af, je kan hem nog uitzetten')
+    sleep(3)
+    print('is alarm ringing', isAlarmRinging)
 
-    if eval(isAlarmRinging):
-        print('DAAA DUUU DAAA DUUU')
+    if isAlarmRinging:
+        print('DAAA DUUU DAA DUU')
         GPIO.output(alarmPin, GPIO.HIGH)
-        break
+    else:
+        print('ja hij is dus uit')
 
-def toggleButtonValue(c):
-    print('uit please')
+
+def zetAlarmUit(c):
+    print('uit please') 
+    global isAlarmRinging
+    isAlarmRinging = False
     GPIO.output(alarmPin, GPIO.LOW)
     GPIO.output(startPin, GPIO.LOW)
 
 
-GPIO.add_event_detect(btnPin, GPIO.RISING, callback=toggleButtonValue)
+
+while True:
+    global isAlarmRinging
+    client, addr = server.accept()
+    client.send('True'.encode())
+    isAlarmRinging = eval(client.recv(1024))
+    
+    if isAlarmRinging:
+        laatAlarmAfGaan()
+        break
 
 
-server.close()
+
 input()
-
-
